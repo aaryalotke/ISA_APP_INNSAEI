9295a53ea74ba973862149299970153cba87af7b
Sania Khan <96515128+saniakhan122@users.noreply.github.com>
2023-09-27 22:43:12 +0530

    Update otp_n_members.dart

--- frontend/lib/otp_n_members.dart
+++ frontend/lib/otp_n_members.dart
@@ -172,7 +172,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               PageRouteBuilder(
                                   pageBuilder: (_, a, b) =>
                                       homePage(widget.access),
-                                  transitionDuration: Duration(seconds: 2),
+                                  transitionDuration: Duration(seconds: 1),
                                   transitionsBuilder: (_, a, __, c) =>
                                       FadeTransition(
                                         opacity: a,
@@ -236,7 +236,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         //   context,
                         //   PageRouteBuilder(
                         //       pageBuilder: (_, a, b) => homePage(),
-                        //       transitionDuration: Duration(seconds: 2),
+                        //       transitionDuration: Duration(seconds: 1),
                         //       transitionsBuilder: (_, a, __, c) =>
                         //           FadeTransition(
                         //             opacity: a,
